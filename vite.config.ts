730f36cc46549b4cd9d3a38bedc151b8a3187040
Vincent Bai <vincebai96@gmail.com>
2025-08-25 15:21:05 -0700

    Try to fix gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  base: "/inhouse-randomizer/",
   plugins: [
+    react(),
     tailwindcss(),
   ],
 })
